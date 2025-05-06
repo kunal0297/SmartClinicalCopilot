c5fb9a812b5191d32505f17b337777e7a38a514a
Kunal Pandey <kunalpandey0297@gmail.com>
2025-05-06 13:00:17 +0530

    Update App.tsx

--- frontend/public/src/App.tsx
+++ frontend/public/src/App.tsx
@@ -1,16 +1,13 @@
-// src/App.tsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 
-const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
